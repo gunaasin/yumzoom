d1509a5cd11e28d42113aaef425e42196f905adf
gunaasin <guna.asin06@gmail.com>
2025-02-19 14:47:38 +0530

    production-live

--- scripts/end-point.js
+++ scripts/end-point.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:8080/api/v1';
+const API = 'https://yum-zoom.onrender.com/api/v1';
 export default API;
 
-export const W_API  = "http://localhost:8080/ws";
+export const W_API  = "https://yum-zoom.onrender.com/ws";
